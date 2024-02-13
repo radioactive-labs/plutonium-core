0d76538fccc713964d4ef7a7dc648543f00856f4
Stefan Froelich <sfroelich01@gmail.com>
2024-02-13 23:19:03 +0000

    improve routing

--- lib/plutonium/packaging/app.rb
+++ lib/plutonium/packaging/app.rb
@@ -35,7 +35,7 @@ module Plutonium
           # e.g. /blogs/1 and blogs/comments cause an issue if Blog is registered before Blogs::Comment
           # attempting to load blogs/comments routes to blogs/:id which fails with a 404
           # Reverse sorting ensures that nested resources are registered first
-          registered_resources = resource_register.map(&:to_s).sort.reverse
+          registered_resources = resource_register.map(&:to_s).sort.reverse.map(&:constantize)
 
           # debugger
 
@@ -49,23 +49,19 @@ module Plutonium
           routes.draw do
             route_drawer = -> {
               registered_resources.each do |resource|
-                resource_name = resource.to_s #.classify
-
-                resource_module = resource_name.deconstantize
-                resource_module_underscored = resource_module&.underscore
-
-                resource_name_plural = resource_name.pluralize
-                resource_name_plural_underscored = resource_name_plural.underscore.tr("/", "_")
-                resource_controller = resource_name_plural.demodulize.underscore
-
-                resources_path = resource_name_plural.underscore
+                resource_name = resource.to_s
+                resource_module = resource_name.deconstantize&.underscore
+                resource_controller = resource_name.pluralize.demodulize.underscore
 
                 route_opts = {}
-                if resource_module_underscored.present?
-                  route_opts[:module] = resource_module_underscored
+                if resource_module.present?
+                  route_opts[:module] = resource_module
                   route_opts[:controller] = resource_controller
-                  route_opts[:path] = resources_path
+                  route_opts[:path] = resource.model_name.collection
                 end
+
+                resources resource.model_name.plural, **route_opts
+
                 # route = <<~TILDE
                 #   concern :#{resource_name_underscored}_routes do
                 #     #{resource_name_underscored}_concerns = %i[]
@@ -78,7 +74,15 @@ module Plutonium
                 #   admin_resource_routes << :#{resource_name_underscored}_routes
                 # TILDE
 
-                resources resource_name_plural_underscored, **route_opts
+                # TODO: test this with uncountables
+                # if resource.model_name.uncountable?
+                #   # https://stackoverflow.com/questions/6476763/rails-3-route-appends-index-to-route-name
+                #   resource resource.model_name.singular, **route_opts
+                #   get "#{resource.model_name.collection}_index", action: :index,
+                #                                       as: resource.model_name.route_key,
+                #                                       **route_opts.slice(:module, :controller)
+                # else
+                # end
               end
             }
 
