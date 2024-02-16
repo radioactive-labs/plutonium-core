18bb4f7c0b301dbbc56f41348aeb35d67790d96f
Stefan Froelich <sfroelich01@gmail.com>
2024-02-16 17:25:18 +0000

    replace form_for override with custom form helper

--- lib/plutonium/helpers/form_helper.rb
+++ lib/plutonium/helpers/form_helper.rb
@@ -3,13 +3,11 @@ module Plutonium
     module FormHelper
       include ActionView::Helpers::FormHelper
 
-      alias_method :pu_overridden_form_for, :form_for
-
       #
       # Override the original form_for helper to disable turbo forms by default if not
       # explicitly opted into
       #
-      def form_for(record, options = {}, &block)
+      def resource_form_for(record, options = {}, &block)
         turbo_frame = options.key?(:turbo_frame) ? options[:turbo_frame] : "_top"
         options = {
           html: {
@@ -19,7 +17,7 @@ module Plutonium
           }
         }.deep_merge! options
 
-        pu_overridden_form_for(record, options, &block)
+        simple_form_for(record, options, &block)
       end
     end
   end
