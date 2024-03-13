a3d9504b950e728e5fdfeff1e851822c17af861a
Stefan Froelich <sfroelich01@gmail.com>
2024-03-13 06:07:25 +0000

    clean up buttons

--- lib/plutonium/helpers/action_buttons_helper.rb
+++ lib/plutonium/helpers/action_buttons_helper.rb
@@ -3,10 +3,10 @@ module Plutonium
     module ActionButtonsHelper
       def generic_action_button(url, label:, icon:, button_class:, method:, confirm:, turbo_frame:)
         if method == :get
-          resource_component :button, to: url, icon:, label:, size: :xs, color: button_class.to_sym, data: {turbo_frame:}
+          resource_component :button, to: url, icon:, label:, size: :xs, color: button_class.to_sym, classes: "basis-1/6", data: {turbo_frame:}
         else
-          form_for :action, url:, method:, turbo_frame:, html: {class: "", data: {turbo_confirm: confirm}} do
-            resource_component :button, icon:, label:, size: :xs, color: button_class.to_sym
+          form_for :action, url:, method:, turbo_frame:, html: {class: "basis-1/6 me-2", data: {turbo_confirm: confirm}} do
+            resource_component :button, icon:, label:, size: :xs, color: button_class.to_sym, classes: "w-full "
           end
         end
       end
