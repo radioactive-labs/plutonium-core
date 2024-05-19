c6bddce9735773513bc3bc33668e5ba255a122a1
Stefan Froelich <sfroelich01@gmail.com>
2024-05-19 12:38:22 +0000

    force page reloads when assets change

--- lib/plutonium/config.rb
+++ lib/plutonium/config.rb
@@ -5,12 +5,12 @@ module Plutonium
   module Config
     mattr_accessor :stylesheet_tag
     @@stylesheet_tag = ->(view_context) {
-      "<link rel=\"stylesheet\" href=\"#{Plutonium.stylesheet_link}\" />"
+      "<link rel=\"stylesheet\" href=\"#{Plutonium.stylesheet_link}\" data-turbo-track=\"reload\" />"
     }
 
     mattr_accessor :script_tag
     @@script_tag = ->(view_context) {
-      "<script src=\"#{Plutonium.script_link}\"></script>"
+      "<script src=\"#{Plutonium.script_link}\" data-turbo-track=\"reload\"></script>"
     }
   end
 end
