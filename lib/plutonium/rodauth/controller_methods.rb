f7faac890654ac8537ecb7a4e65e2506a171d0c9
Stefan Froelich <sfroelich01@gmail.com>
2024-05-27 22:57:39 +0000

    add default root_path for rodauth controllers

--- lib/plutonium/rodauth/controller_methods.rb
+++ lib/plutonium/rodauth/controller_methods.rb
@@ -6,13 +6,17 @@ module Plutonium
       included do
         layout "rodauth"
         append_view_path File.expand_path("app/views", Plutonium.root)
-        helper_method :application_name
+        helper_method :application_name, :root_path
       end
 
       private
 
       def application_name
         Plutonium.application_name
+      end
+
+      def root_path
+        "/"
       end
     end
   end
