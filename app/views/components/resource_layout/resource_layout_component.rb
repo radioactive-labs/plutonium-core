f7efbac2b9d32de4ff6096c4b65f10870649949b
Stefan Froelich <sfroelich01@gmail.com>
2024-06-10 17:34:04 +0000

    remove w-full from containers

--- app/views/components/resource_layout/resource_layout_component.rb
+++ app/views/components/resource_layout/resource_layout_component.rb
@@ -9,9 +9,9 @@ module PlutoniumUi
 
     option :page_title
     option :lang
-    option :body_classname, default: -> { "antialiased min-h-screen w-full bg-gray-50 dark:bg-gray-900" }
-    option :main_classname, default: -> { "p-4" }
-    option :header_adjustment, default: -> { "pt-20 min-h-screen w-full" }
+    option :body_classname, default: -> { "antialiased min-h-screen bg-gray-50 dark:bg-gray-900" }
+    option :main_classname, default: -> { "p-4 min-h-screen" }
+    option :header_adjustment, default: -> { "pt-20" }
     option :sidebar_adjustment, default: -> { "lg:ml-64" }
     option :default_turbo_tag, default: -> { true }
     option :default_fonts_tag, default: -> { true }
