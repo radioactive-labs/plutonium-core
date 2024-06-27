c4a52602392f42b42712396e52ff2f300030a0c1
Stefan Froelich <sfroelich01@gmail.com>
2024-06-27 21:54:46 +0000

    tweak form appearance

--- lib/plutonium/helpers/form_helper.rb
+++ lib/plutonium/helpers/form_helper.rb
@@ -7,13 +7,17 @@ module Plutonium
 
       def resource_form_for(record, **options, &block)
         options[:url] ||= resource_url_args_for(record, action: record.new_record? ? :create : :update)
-        options[:builder] ||= PlutoniumUi::FormBuilder
-        options[:wrapper] ||= :default_resource_form
-        options[:html] ||= {}
-        options[:html][:novalidate] = false unless options[:html].key?(:novalidate)
+        options = build_form_options(options)
 
         with_resource_form_field_error_proc do
           form_for(record, options, &block)
+        end
+      end
+
+      def resource_form_with(**options, &block)
+        options = build_form_options(options)
+        with_resource_form_field_error_proc do
+          form_with(**options, &block)
         end
       end
 
@@ -23,6 +27,14 @@ module Plutonium
       end
 
       private
+
+      def build_form_options(options)
+        options[:builder] ||= PlutoniumUi::FormBuilder
+        options[:wrapper] ||= :default_resource_form
+        options[:html] ||= {}
+        options[:html][:novalidate] = false unless options[:html].key?(:novalidate)
+        options
+      end
 
       def with_resource_form_field_error_proc
         # borrowed from https://github.com/heartcombo/simple_form/blob/main/lib/simple_form/action_view_extensions/form_helper.rb#L40C1-L50C10
