5eecc891a4a4eb355d30662e37b2a3bcc55b154f
Stefan Froelich <sfroelich01@gmail.com>
2024-07-19 13:58:43 +0000

    support connecting non plutonium resources

--- lib/generators/pu/res/conn/conn_generator.rb
+++ lib/generators/pu/res/conn/conn_generator.rb
@@ -19,6 +19,7 @@ module Pu
 
         Plutonium.eager_load_rails!
         available_resources = source_module.constantize.descendants.map(&:to_s).sort
+        error "No resources found" if available_resources.blank?
         selected_resources = prompt.multi_select("Select resources", available_resources)
 
         @app_namespace = select_app.camelize
@@ -27,9 +28,9 @@ module Pu
           @resource_class = resource
 
           template "app/controllers/resource_controller.rb", "packages/#{package_namespace}/app/controllers/#{package_namespace}/#{resource.pluralize.underscore}_controller.rb"
-          # template "app/policies/resource_policy.rb", "packages/#{package_namespace}/app/policies/#{package_namespace}/#{resource.underscore}_policy.rb"
-          # template "app/presenters/resource_presenter.rb", "packages/#{package_namespace}/app/presenters/#{package_namespace}/#{resource.underscore}_presenter.rb"
-          # template "app/query_objects/resource_query_object.rb", "packages/#{package_namespace}/app/query_objects/#{package_namespace}/#{resource.underscore}_query_object.rb"
+          template "app/policies/resource_policy.rb", "packages/#{package_namespace}/app/policies/#{package_namespace}/#{resource.underscore}_policy.rb" unless expected_parent_policy
+          template "app/presenters/resource_presenter.rb", "packages/#{package_namespace}/app/presenters/#{package_namespace}/#{resource.underscore}_presenter.rb" unless expected_parent_presenter
+          template "app/query_objects/resource_query_object.rb", "packages/#{package_namespace}/app/query_objects/#{package_namespace}/#{resource.underscore}_query_object.rb" unless expected_parent_query_object
 
           insert_into_file "packages/#{package_namespace}/config/routes.rb",
             indent("register_resource ::#{resource}\n", 2),
@@ -51,9 +52,53 @@ module Pu
         app_namespace.underscore
       end
 
+      def expected_parent_controller
+        expected_parent_controller = "::#{resource_class.pluralize}Controller".safe_constantize
+        expected_parent_controller if expected_parent_controller.present? && expected_parent_controller < ::ResourceController
+      end
+
       def parent_controller
-        resource_controller = "::#{resource_class.pluralize}Controller".safe_constantize
-        (resource_controller.present? && resource_controller < ::ResourceController) ? resource_controller : "#{app_namespace}::ResourceController"
+        expected_parent_controller || "#{app_namespace}::ResourceController"
+      end
+
+      def expected_parent_policy
+        expected_parent_policy = "::#{resource_class.pluralize}Policy".safe_constantize
+        expected_parent_policy if expected_parent_policy.present? && expected_parent_policy < ::ResourcePolicy
+      end
+
+      def parent_policy
+        expected_parent_policy || "ResourcePolicy"
+      end
+
+      def expected_parent_presenter
+        expected_parent_presenter = "::#{resource_class.pluralize}Presenter".safe_constantize
+        expected_parent_presenter if expected_parent_presenter.present? && expected_parent_presenter < ::ResourcePresenter
+      end
+
+      def parent_presenter
+        expected_parent_presenter || "ResourcePresenter"
+      end
+
+      def expected_parent_query_object
+        expected_parent_query_object = "::#{resource_class.pluralize}QueryObject".safe_constantize
+        expected_parent_query_object if expected_parent_query_object.present? && expected_parent_query_object < ::ResourceQueryObject
+      end
+
+      def parent_query_object
+        expected_parent_query_object || "ResourceQueryObject"
+      end
+
+      def attributes
+        resource_klass = resource_class.constantize
+        unwanted_attrs = [
+          resource_klass.primary_key.to_sym, # primary_key
+          :created_at, :updated_at # timestamps
+        ]
+        resource_klass.content_columns.filter_map { |col|
+          next if unwanted_attrs.include? col.name.to_sym
+
+          PlutoniumGenerators::ModelGeneratorBase::GeneratedAttribute.parse resource_class, "#{col.name}:#{col.type}"
+        }
       end
     end
   end
