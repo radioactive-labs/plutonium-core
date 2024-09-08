418c71323d3f11977b94121a9ac3f9a2263688ed
Stefan Froelich <sfroelich01@gmail.com>
2024-09-08 22:30:20 +0000

    remove superflous installation files

--- lib/generators/pu/core/install/templates/config/initializers/plutonium.rb
+++ lib/generators/pu/core/install/templates/config/initializers/plutonium.rb
@@ -6,10 +6,3 @@ Plutonium.configure do |config|
   # config.assets.logo = "logo.png"
   # Configure plutonium above.
 end
-
-Rails.application.config.to_prepare do
-  # Register components here
-
-  # e.g
-  # Plutonium::Core::Fields::Renderers::Factory.map_type :mapped_collection, to: Fields::Renderers::MappedCollectionRenderer
-end
