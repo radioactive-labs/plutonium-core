9dd480de5beea7749a4631afd5ec120f3456cae0
TheDumbTechGuy <sfroelich01@gmail.com>
2024-09-25 05:38:24 +0000

    update phlexi::display

--- lib/plutonium/ui/display/theme.rb
+++ lib/plutonium/ui/display/theme.rb
@@ -7,17 +7,18 @@ module Plutonium
         def self.theme
           super.merge({
             base: "relative bg-white dark:bg-gray-800 shadow-md sm:rounded-lg my-3",
+            value_wrapper: "max-h-[300px] overflow-y-auto",
             fields_wrapper: "p-6 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-6 gap-y-10 grid-flow-row-dense",
             label: "text-base font-bold text-gray-500 dark:text-gray-400 mb-1",
             description: "text-sm text-gray-400 dark:text-gray-500",
             placeholder: "text-md text-gray-500 dark:text-gray-300 mb-1 italic",
-            string: "max-h-[300px] overflow-y-auto text-md text-gray-900 dark:text-white mb-1 whitespace-pre-line",
-            text: "max-h-[300px] overflow-y-auto text-md text-gray-900 dark:text-white mb-1 whitespace-pre-line",
+            string: "text-md text-gray-900 dark:text-white mb-1 whitespace-pre-line",
+            text: "text-md text-gray-900 dark:text-white mb-1 whitespace-pre-line",
             link: "text-primary-600 dark:text-primary-500 whitespace-pre-line",
             color: "flex items-center text-md text-gray-900 dark:text-white mb-1 whitespace-pre-line",
             color_indicator: "w-10 h-10 rounded-full mr-2", # max-h-fit
             email: "flex items-center text-md text-primary-600 dark:text-primary-500 mb-1 whitespace-pre-line",
-            json: "max-h-[300px] overflow-y-auto text-sm text-gray-900 dark:text-white mb-1 whitespace-pre font-mono shadow-inner p-4",
+            json: "text-sm text-gray-900 dark:text-white mb-1 whitespace-pre font-mono shadow-inner p-4",
             prefixed_icon: "w-8 h-8 mr-2"
           })
         end
--- lib/plutonium/ui/table/display_theme.rb
+++ lib/plutonium/ui/table/display_theme.rb
@@ -6,13 +6,13 @@ module Plutonium
       class DisplayTheme < Phlexi::Table::DisplayTheme
         def self.theme
           super.merge({
-            string: "max-h-[150px] overflow-y-auto",
+            value_wrapper: "max-h-[150px] overflow-y-auto",
             prefixed_icon: "w-4 h-4 mr-1",
             link: "text-primary-600 dark:text-primary-500",
             color: "flex items-center",
             color_indicator: "w-10 h-10 rounded-full mr-2",
             email: "flex items-center text-primary-600 dark:text-primary-500 whitespace-nowrap",
-            json: "max-h-[150px] overflow-y-auto whitespace-pre font-mono shadow-inner p-4"
+            json: " whitespace-pre font-mono shadow-inner p-4"
           })
         end
       end
