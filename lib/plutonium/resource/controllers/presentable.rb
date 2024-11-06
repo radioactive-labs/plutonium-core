898f40a0e780a5084d12c1c3936675bfbf9696e8
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-06 23:44:34 +0000

    treat scoped params differently for display vs input

--- lib/plutonium/resource/controllers/presentable.rb
+++ lib/plutonium/resource/controllers/presentable.rb
@@ -5,7 +5,6 @@ module Plutonium
         extend ActiveSupport::Concern
 
         included do
-          helper_method :presentable_attributes
           helper_method :build_form, :build_detail, :build_collection
         end
 
@@ -14,9 +13,21 @@ module Plutonium
         def presentable_attributes
           @presentable_attributes ||= begin
             presentable_attributes = permitted_attributes
-            presentable_attributes -= [scoped_entity_param_key, :"#{scoped_entity_param_key}_id"] if scoped_to_entity?
-            presentable_attributes -= [parent_input_param, :"#{parent_input_param}_id"] if current_parent.present?
+            if current_parent
+              presentable_attributes -= [parent_input_param, :"#{parent_input_param}_id"]
+            elsif scoped_to_entity?
+              presentable_attributes -= [scoped_entity_param_key, :"#{scoped_entity_param_key}_id"]
+            end
             presentable_attributes
+          end
+        end
+
+        def submittable_attributes
+          @submittable_attributes ||= begin
+            submittable_attributes = permitted_attributes
+            submittable_attributes -= [parent_input_param, :"#{parent_input_param}_id"] if current_parent
+            submittable_attributes -= [scoped_entity_param_key, :"#{scoped_entity_param_key}_id"] if scoped_to_entity?
+            submittable_attributes
           end
         end
 
@@ -29,7 +40,7 @@ module Plutonium
         end
 
         def build_form(record = resource_record)
-          current_definition.form_class.new(record, resource_fields: presentable_attributes, resource_definition: current_definition)
+          current_definition.form_class.new(record, resource_fields: submittable_attributes, resource_definition: current_definition)
         end
       end
     end
