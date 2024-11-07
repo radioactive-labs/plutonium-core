624023ed7a49c30b546acae9bfa18d21b1949a98
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-07 00:00:41 +0000

    scope resources to entity before fetching by param

--- lib/plutonium/resource/controller.rb
+++ lib/plutonium/resource/controller.rb
@@ -136,8 +136,12 @@ module Plutonium
         @current_parent ||= begin
           parent_route_key = parent_route_param.to_s.gsub(/_id$/, "").to_sym
           parent_class = current_engine.resource_register.route_key_lookup[parent_route_key]
-          parent_scope = parent_class.from_path_param(params[parent_route_param])
-          parent_scope = parent_scope.associated_with(current_scoped_entity) if scoped_to_entity?
+          parent_scope = if scoped_to_entity?
+            parent_class.associated_with(current_scoped_entity)
+          else
+            parent_class.all
+          end
+          parent_scope = parent_scope.from_path_param(params[parent_route_param])
           current_parent = parent_scope.first!
           authorize! current_parent, to: :read?
           current_parent
