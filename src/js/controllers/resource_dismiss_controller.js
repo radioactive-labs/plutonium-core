c48d268bb9ea624e66bfc19f0bfab8939895b237
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-10 02:46:10 +0000

    modify dismiss controller to remove element during dimiss (#10)
    this ensures that during page refreshes, a new controller is bound fixing the alerts part of https://github.com/radioactive-labs/plutonium-core/issues/8
    
    it also removes the dependency on flowbite and is now a proper hotwire component

--- src/js/controllers/resource_dismiss_controller.js
+++ src/js/controllers/resource_dismiss_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import { Dismiss } from 'flowbite';
-
 
 // Connects to data-controller="resource-dismiss"
 export default class extends Controller {
-  static targets = ["trigger", "target"]
-
   static values = {
     after: Number,
   }
@@ -13,14 +9,9 @@ export default class extends Controller {
   connect() {
     console.log(`resource-dismiss connected: ${this.element}`)
 
-    // https://flowbite.com/docs/components/alerts/#javascript-behaviour
-    this.dismiss = new Dismiss(this.targetTarget, this.triggerTarget);
-
-    console.log(this.hasAfterValue)
-    console.log(this.afterValue)
     if (this.hasAfterValue && this.afterValue > 0) {
       this.autoDismissTimeout = setTimeout(() => {
-        this.hide()
+        this.dismiss()
         this.autoDismissTimeout = null
       }, this.afterValue);
     }
@@ -29,11 +20,10 @@ export default class extends Controller {
   disconnect() {
     if (this.autoDismissTimeout) clearTimeout(this.autoDismissTimeout)
 
-    this.dismiss = null
     this.autoDismissTimeout = null
   }
 
-  hide() {
-    this.dismiss.hide()
+  dismiss() {
+    this.element.remove()
   }
 }
