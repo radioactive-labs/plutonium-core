612460172c39e5ed57a50ec27f18454eff5ef258
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-17 15:18:26 +0000

    rodauth param result is never nil

--- lib/rodauth/features/case_insensitive_login.rb
+++ lib/rodauth/features/case_insensitive_login.rb
@@ -1,17 +1,21 @@
 require "rodauth"
 
+# A plugin to enable case insensitive logins on Rodauth.
+# It does that by downcasing any login inputs.
+# Should not be enabled on existing installations unless logins are downcased in the database.
+# See https://github.com/jeremyevans/rodauth/discussions/451
 module Rodauth
   Feature.define(:case_insensitive_login, :CaseInsensitiveLogin) do
     def param(key)
       if [login_param, login_confirm_param].include?(key)
-        super&.downcase
+        super.downcase
       else
         super
       end
     end
 
     def account_from_login(login)
-      super(login&.downcase)
+      super(login.downcase)
     end
   end
 end
