fce197636068f76c031b70258641e1bfbdc7ad56
TheDumbTechGuy <sfroelich01@gmail.com>
2024-12-27 19:24:55 +0000

    remove plutonium icons

--- lib/plutonium/helpers/application_helper.rb
+++ lib/plutonium/helpers/application_helper.rb
@@ -4,14 +4,6 @@ module Plutonium
       def application_name
         Plutonium.application_name
       end
-
-      # Renders an icon using the Plutonium Icons library.
-      #
-      # @param icon [Symbol, String] The name or identifier of the icon to render.
-      # @return [String] The HTML-safe string for the rendered icon.
-      def render_icon(icon, **)
-        Plutonium::Icons.render(icon, **)
-      end
     end
   end
 end
