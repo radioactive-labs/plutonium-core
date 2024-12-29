c016c855c816f6b25989f72a76e5ed53116da35f
TheDumbTechGuy <sfroelich01@gmail.com>
2024-12-29 00:49:55 +0000

    add management link to frame navigator panel

--- lib/plutonium/ui/frame_navigator_panel.rb
+++ lib/plutonium/ui/frame_navigator_panel.rb
@@ -20,6 +20,24 @@ module Plutonium
         end
       end
 
+      class PanelLink < Plutonium::UI::Component::Base
+        def initialize(label:, icon:, href:)
+          @label = label
+          @icon = icon
+          @href = href
+        end
+
+        def view_template
+          a(
+            title: @label,
+            class: "text-gray-600 dark:text-gray-300",
+            href: @href
+          ) {
+            render @icon.new(class: "w-6 h-6")
+          }
+        end
+      end
+
       class PanelContent < Plutonium::UI::Component::Base
         def initialize(src:)
           @src = src
@@ -44,6 +62,7 @@ module Plutonium
             panel.with_item PanelItem.new(label: "Home", icon: Phlex::TablerIcons::Home2, data_frame_navigator_target: %(homeButton))
             panel.with_item PanelItem.new(label: "Back", icon: Phlex::TablerIcons::ChevronLeft, data_frame_navigator_target: %(backButton))
             panel.with_item PanelItem.new(label: "Refresh", icon: Phlex::TablerIcons::RefreshDot, data_frame_navigator_target: %(refreshButton))
+            panel.with_item PanelLink.new(label: "Manage", icon: Phlex::TablerIcons::LayoutDashboard, href: @src)
             panel.with_content PanelContent.new(src: @src)
           end
         end
