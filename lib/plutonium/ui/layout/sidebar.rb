f791dab60a682eeb5e99ec67ab43b8c5e69d5ba3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-09 12:50:48 +0000

    feat: Show the color mode controls on sidebar for small screen devices #16

--- lib/plutonium/ui/layout/sidebar.rb
+++ lib/plutonium/ui/layout/sidebar.rb
@@ -44,7 +44,7 @@ module Plutonium
 
         # @private
         def render_color_mode_controls
-          div(class: "hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20 border-r border-gray-200 dark:border-gray-700") do
+          div(class: "absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full flex bg-white dark:bg-gray-800 z-20 border-r border-gray-200 dark:border-gray-700") do
             render ColorModeSelector.new
           end
         end
