4d17244876d7570eeeb2f2564413ff8b3b938cd5
TheDumbTechGuy <sfroelich01@gmail.com>
2025-02-19 15:58:25 +0000

    improve rodauth layout customizability

--- lib/plutonium/ui/layout/rodauth_layout.rb
+++ lib/plutonium/ui/layout/rodauth_layout.rb
@@ -11,18 +11,26 @@ module Plutonium
         end
 
         def main_attributes = mix(super, {
-          class: "flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+          class: "flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0"
         })
 
         def render_content(&)
-          link_to root_path, class: "flex items-center text-2xl font-semibold text-gray-900 dark:text-white" do
-            helpers.resource_logo_tag classname: "w-24 h-24 mr-2"
-          end
+          render_logo
 
           div(class: "w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700") {
             div(class: "p-6 space-y-4 md:space-y-6 sm:p-8", &)
           }
 
+          render_links
+        end
+
+        def render_logo
+          link_to root_path, class: "flex items-center text-2xl font-semibold text-gray-900 dark:text-white" do
+            helpers.resource_logo_tag classname: "w-24 h-24 mr-2"
+          end
+        end
+
+        def render_links
           div(class: "mt-4 flex items-center font-medium text-secondary-600 dark:text-secondary-400 hover:underline") {
             render Phlex::TablerIcons::Home2.new
             link_to "Home", root_path, class: "font-medium text-secondary-600 dark:text-secondary-400"
