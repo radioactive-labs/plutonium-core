dc7c6dac8f107f944330d0dc9b33396a7a662ed2
TheDumbTechGuy <sfroelich01@gmail.com>
2025-07-01 10:12:58 +0000

    support modals in action redirects

--- lib/plutonium/interaction/response/redirect.rb
+++ lib/plutonium/interaction/response/redirect.rb
@@ -12,7 +12,18 @@ module Plutonium
         # @param controller [ActionController::Base] The controller instance.
         # @return [void]
         def execute(controller)
-          controller.redirect_to(*@args, **@options)
+          controller.instance_eval do
+            url = url_for(*@args)
+
+            format.any { redirect_to(url, **@options) }
+            if helpers.current_turbo_frame == "remote_modal"
+              format.turbo_stream do
+                render turbo_stream: [
+                  helpers.turbo_stream_redirect(url)
+                ]
+              end
+            end
+          end
         end
       end
     end
