4a50c5e6f386e368fc09af700c9b9030acdb1418
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-07-12 17:23:21 +0000

    fix: Enhance flatpickr to attach to modals correctly

--- src/js/controllers/flatpickr_controller.js
+++ src/js/controllers/flatpickr_controller.js
@@ -1,35 +1,46 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    this.picker = new flatpickr(this.element, this.#buildOptions())
-    this.element.setAttribute("data-action", "turbo:morph-element->flatpickr#reconnect")
+    this.modal = document.querySelector("[data-controller=remote-modal]");
+
+    this.picker = new flatpickr(this.element, this.#buildOptions());
+
+    this.element.setAttribute(
+      "data-action",
+      "turbo:morph-element->flatpickr#reconnect"
+    );
   }
 
   disconnect() {
     if (this.picker) {
-      this.picker.destroy()
-      this.picker = null
+      this.picker.destroy();
+      this.picker = null;
     }
   }
 
   reconnect() {
-    this.disconnect()
-    this.connect()
+    this.disconnect();
+    this.connect();
   }
 
   #buildOptions() {
-    let options = { altInput: true }
+    let options = { altInput: true };
+
     if (this.element.attributes.type.value == "datetime-local") {
-      options.enableTime = true
-    }
-    else if (this.element.attributes.type.value == "time") {
-      options.enableTime = true
-      options.noCalendar = true
+      options.enableTime = true;
+    } else if (this.element.attributes.type.value == "time") {
+      options.enableTime = true;
+      options.noCalendar = true;
       // options.time_24hr = true
       // options.altFormat = "H:i"
     }
-    return options
+
+    if (this.modal) {
+      options.appendTo = this.modal;
+    }
+
+    return options;
   }
 }
