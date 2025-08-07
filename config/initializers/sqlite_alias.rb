fce4542835a6153ad4caaf3fe24f2a8f87f5586b
Bless Ziamah <tziamah1@gmail.com>
2025-08-07 15:30:56 +0000

    fix: prevent SQLite adapter error when using non-SQLite databases (#42)

--- config/initializers/sqlite_alias.rb
+++ config/initializers/sqlite_alias.rb
@@ -1,5 +1,7 @@
 # Alias json to jsonb in SQLite migrations
 ActiveSupport.on_load(:active_record) do
+  next unless ActiveRecord::Base.connection.adapter_name.downcase.include?("sqlite")
+
   ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition.class_eval do
     def jsonb(*args, **options)
       json(*args, **options)
