9202e3237db510690e4711f0cb7d4ca823bf10c1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-08-08 17:11:51 +0000

    fix: Prevent error by checking for SQLite3 adapter before defining jsonb alias

--- config/initializers/sqlite_alias.rb
+++ config/initializers/sqlite_alias.rb
@@ -1,4 +1,6 @@
 # Alias json to jsonb in SQLite migrations
+return unless defined?(ActiveRecord::ConnectionAdapters::SQLite3)
+
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition.class_eval do
     def jsonb(*args, **options)
