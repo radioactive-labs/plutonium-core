eb71edf4b55afe8fdc5c5b4adc29de8bba3f3fa9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-08-11 09:26:02 +0000

    Add check for SQLite3 adapter before monkey-patching JSON alias

--- config/initializers/sqlite_alias.rb
+++ config/initializers/sqlite_alias.rb
@@ -1,7 +1,8 @@
 # Alias json to jsonb in SQLite migrations
-ActiveSupport.on_load(:active_record) do
-  next unless ActiveRecord::Base.connection.adapter_name.downcase.include?("sqlite")
 
+return unless defined?(ActiveRecord::ConnectionAdapters::SQLite3)
+
+ActiveSupport.on_load(:active_record) do
   ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition.class_eval do
     def jsonb(*args, **options)
       json(*args, **options)
