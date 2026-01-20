ce66824ff7bd9df71b40429c800560157a17a519
TheDumbTechGuy <sfroelich01@gmail.com>
2026-01-20 04:41:15 +0000

    chore: standardrb

--- test/dummy/packages/author_portal/lib/engine.rb
+++ test/dummy/packages/author_portal/lib/engine.rb
@@ -1,6 +1,7 @@
 module AuthorPortal
   class Engine < Rails::Engine
     include Plutonium::Portal::Engine
+
     # add concerns above.
 
     config.after_initialize do
--- test/dummy/packages/demo_features/db/migrate/20260118222654_create_demo_features_morph_demos.rb
+++ test/dummy/packages/demo_features/db/migrate/20260118222654_create_demo_features_morph_demos.rb
@@ -1,7 +1,7 @@
 class CreateDemoFeaturesMorphDemos < ActiveRecord::Migration[8.1]
   def change
     create_table :demo_features_morph_demos do |t|
-      t.belongs_to :category, null: false, foreign_key: {:to_table=>:demo_features_categories}
+      t.belongs_to :category, null: false, foreign_key: {to_table: :demo_features_categories}
       t.integer :record_type, null: false
       t.string :name, null: false
       t.string :status, null: false
