69d6f748c5840600b224109c92402904edc3d58f
TheDumbTechGuy <sfroelich01@gmail.com>
2026-01-29 23:21:32 +0000

    fix(test): add variants to product policy permitted attributes

--- test/dummy/packages/demo_features/app/policies/demo_features/product_policy.rb
+++ test/dummy/packages/demo_features/app/policies/demo_features/product_policy.rb
@@ -12,7 +12,7 @@ class DemoFeatures::ProductPolicy < DemoFeatures::ResourcePolicy
   # Core attributes
 
   def permitted_attributes_for_create
-    [:name, :sku, :slug, :description, :notes, :stock_count, :weight, :price, :compare_at_price, :active, :featured, :taxable, :release_date, :discontinue_date, :last_restocked_at, :published_at, :available_from_time, :available_until_time, :metadata, :specifications, :status, :category]
+    [:name, :sku, :slug, :description, :notes, :stock_count, :weight, :price, :compare_at_price, :active, :featured, :taxable, :release_date, :discontinue_date, :last_restocked_at, :published_at, :available_from_time, :available_until_time, :metadata, :specifications, :status, :category, :variants]
   end
 
   def permitted_attributes_for_read
@@ -22,6 +22,6 @@ class DemoFeatures::ProductPolicy < DemoFeatures::ResourcePolicy
   # Associations
 
   def permitted_associations
-    %i[]
+    %i[variants]
   end
 end
