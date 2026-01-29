15f61ff16c2b730b87f8480cbd79793386c126b3
TheDumbTechGuy <sfroelich01@gmail.com>
2026-01-29 23:21:17 +0000

    fix(config): wrap SQLite alias in defined? check for compatibility

--- config/initializers/sqlite_alias.rb
+++ config/initializers/sqlite_alias.rb
@@ -1,15 +1,15 @@
 # Alias json to jsonb in SQLite migrations
 
-return unless defined?(ActiveRecord::ConnectionAdapters::SQLite3)
-
 ActiveSupport.on_load(:active_record) do
-  ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition.class_eval do
-    def jsonb(*args, **options)
-      json(*args, **options)
-    end
+  if defined?(ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition)
+    ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition.class_eval do
+      def jsonb(*args, **options)
+        json(*args, **options)
+      end
 
-    def uuid(*args, **options)
-      string(*args, **options)
+      def uuid(*args, **options)
+        string(*args, **options)
+      end
     end
   end
 end
