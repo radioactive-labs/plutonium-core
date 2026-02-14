047ef54b962e63910652c7a612d7c5f0ff68203d
TheDumbTechGuy <sfroelich01@gmail.com>
2026-02-14 15:03:25 +0000

    feat(interaction): add turbo stream support to render response

--- lib/plutonium/interaction/response/render.rb
+++ lib/plutonium/interaction/response/render.rb
@@ -12,7 +12,22 @@ module Plutonium
         # @param controller [ActionController::Base] The controller instance.
         # @return [void]
         def execute(controller)
-          controller.render(*@args, @options)
+          render_args = @args
+          render_options = @options
+
+          controller.instance_eval do
+            respond_to do |format|
+              format.turbo_stream do
+                # For Turbo requests, replace the form with the rendered content
+                render turbo_stream: turbo_stream.replace(
+                  "interaction-form",
+                  view_context.render(*render_args, **render_options)
+                )
+              end
+
+              format.any { render(*render_args, **render_options) }
+            end
+          end
         end
       end
     end
