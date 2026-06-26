dc08490f2de4d35f37894c9d57ec4adce1c8afa0
TheDumbTechGuy <sfroelich01@gmail.com>
2026-06-26 14:48:59 +0000

    test(kanban): align Task 0 test conventions + idempotency case

--- test/plutonium/definition/kanban_index_view_test.rb
+++ test/plutonium/definition/kanban_index_view_test.rb
@@ -2,25 +2,33 @@
 
 require "test_helper"
 
-class KanbanIndexViewTest < ActiveSupport::TestCase
+class Plutonium::Definition::KanbanIndexViewTest < Minitest::Test
   def def_class(&blk)
     Class.new(Plutonium::Resource::Definition) do
       class_eval(&blk) if blk
     end
   end
 
-  test "declaring kanban enables the :kanban view alongside :table" do
+  def test_declaring_kanban_enables_the_kanban_view_alongside_table
     klass = def_class { kanban {} }
     assert_includes klass.defined_index_views, :kanban
     assert_includes klass.defined_index_views, :table
   end
 
-  test "kanban stores the builder block" do
+  def test_kanban_stores_the_builder_block
     klass = def_class { kanban {} }
     assert_kind_of Proc, klass.defined_kanban_block
   end
 
-  test ":kanban is a known view" do
+  def test_kanban_is_a_known_view
     assert_includes Plutonium::Definition::IndexViews::KNOWN_VIEWS, :kanban
+  end
+
+  def test_calling_kanban_twice_does_not_duplicate_kanban_in_index_views
+    klass = def_class do
+      kanban {}
+      kanban {}
+    end
+    assert_equal 1, klass.defined_index_views.count(:kanban)
   end
 end
